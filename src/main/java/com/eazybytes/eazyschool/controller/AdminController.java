83bb81606177610ecf06c4dc1bd4fb3719db26c1
Swarna Latha <swarnalatha162403@gmail.com>
2025-02-18 22:39:22 +0530

    Finished implementing custom queries in spring data JPA.

--- src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.data.domain.Sort;
 
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
@@ -106,7 +107,9 @@ public class AdminController {
     }
     @GetMapping("/displayCourses")
     public ModelAndView displayCourses(Model model) {
-        List<Courses> courses = coursesRepository.findAll();
+        // List<Courses> courses = coursesRepository.findByOrderByNameDesc();
+        List<Courses> courses = coursesRepository.findAll(Sort.by("name").ascending());
+
         ModelAndView modelAndView = new ModelAndView("courses_secure.html");
         modelAndView.addObject("courses",courses);
         modelAndView.addObject("course", new Courses());
--- src/main/java/com/eazybytes/eazyschool/model/Contact.java
+++ src/main/java/com/eazybytes/eazyschool/model/Contact.java
@@ -1,6 +1,7 @@
 package com.eazybytes.eazyschool.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ColumnResult;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +12,10 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import jakarta.persistence.Id;
+import jakarta.persistence.SqlResultSetMapping;
+import jakarta.persistence.SqlResultSetMappings;
+import jakarta.persistence.*;
+
 
 
 /*
@@ -21,6 +26,29 @@ This makes our code short and clean.
 @Data
 @Entity
 @Table(name = "contact_msg")
+@SqlResultSetMappings({
+        @SqlResultSetMapping(name = "SqlResultSetMapping.count", columns = @ColumnResult(name = "cnt"))
+})
+@NamedQueries({
+    @NamedQuery(name = "Contact.findOpenMsgs",
+            query = "SELECT c FROM Contact c WHERE c.status = :status"),
+    @NamedQuery(name = "Contact.updateMsgStatus",
+            query = "UPDATE Contact c SET c.status = ?1 WHERE c.contactId = ?2")
+})
+@NamedNativeQueries({
+    @NamedNativeQuery(name = "Contact.findOpenMsgsNative",
+            query = "SELECT * FROM contact_msg c WHERE c.status = :status"
+            ,resultClass = Contact.class),
+    @NamedNativeQuery(name = "Contact.findOpenMsgsNative.count",
+            query = "select count(*) as cnt from contact_msg c where c.status = :status",
+            resultSetMapping = "SqlResultSetMapping.count"),
+    /*Spring Data JPA doesn’t support dynamic sorting for native queries.
+    Doing that would require Spring Data to analyze the provided statement and generate
+    the ORDER BY clause in the database-specific dialect. This would be a very complex operation
+    and is currently not supported by Spring Data JPA.*/
+    @NamedNativeQuery(name = "Contact.updateMsgStatusNative",
+            query = "UPDATE contact_msg c SET c.status = ?1 WHERE c.contact_id = ?2")
+})
 public class Contact extends BaseEntity {
 
     @Id
